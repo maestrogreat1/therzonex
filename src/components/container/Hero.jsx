e556b5a4f25b63ba52daf01196d4e4603cca0ff9
maestrogreat1 <ononiwugreat@yahoo.com>
2024-11-28 09:25:23 +0100

    UI refine

--- src/components/container/Hero.jsx
+++ src/components/container/Hero.jsx
@@ -81,7 +81,7 @@ const Hero = ({ handleOrderPopup }) => {
     // </div>
     <div>
       <main className=" mx-auto bg-white overflow-hidden">
-        <section className="hero h-[640px] md:h-[940px] bg-hero bg-center bg-blend-overlay bg-slate-700 bg-cover bg-no-repeat bg-fixed md:rounded-bl-[290px] relative ">
+        <section className="hero h-[640px] md:h-[940px] bg-hero bg-center bg-cover bg-no-repeat bg-fixed md:rounded-bl-[290px] relative ">
         <div className="container mx-auto h-full flex items-center justify-center sm:justify-start">
           {/* text */}
           <div className="hero__text w-[567px] flex flex-col items-center text-center md:text-left sm:items-start mt-10">
